d16325bcf945d4ae870bdb1c999e4cd7719d80f1
Julian Schuh <julez@julez.in>
2014-04-19 18:35:56 +0200

    Started adding (very) basic documentation

--- lib/statusboard/widget.rb
+++ lib/statusboard/widget.rb
@@ -1,5 +1,11 @@
 module Statusboard
+
+	# (Abstract) class that represents a status board widget.
+	# The class must be subclassed for each widget type supported.
 	class WidgetBase
+
+		# Method that renders the widget into a format understandable by
+		# the status board app depending on the configured/fetched data.
 		def render
 			raise "Not implemented."
 		end
