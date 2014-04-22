d80f5360d1f73ca17a45efc3dfd005258d0ce130
Julian Schuh <julez@julez.in>
2014-04-22 18:37:05 +0200

    Moved widget types to own folder

--- lib/statusboard.rb
+++ lib/statusboard.rb
@@ -1,9 +1,9 @@
 require "statusboard/version"
 require "statusboard/dsl/dsl"
 require "statusboard/widget"
-require "statusboard/graph"
-require "statusboard/diy"
-require "statusboard/table"
+require "statusboard/widgets/graph"
+require "statusboard/widgets/diy"
+require "statusboard/widgets/table"
 
 module Statusboard
 	VIEW_PATH = File.join(File.dirname(__FILE__), "statusboard", "views")
