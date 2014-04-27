2089fa5e1f572340c2d9f547e81e71589bd2d6f7
Julian Schuh <julez@julez.in>
2014-04-27 17:19:04 +0200

    Improved code documentation

--- lib/statusboard/main.rb
+++ lib/statusboard/main.rb
@@ -4,8 +4,9 @@ require "statusboard/server"
 require "rack/handler"
 require "rack-handlers"
 
-app = Statusboard::StatusboardServer.new!
+app = Statusboard::StatusboardServer.new! # create a new, _unwrapped_ instance of the server class
 
+# Make the top-level syntax work by delegating the right function calls to the right destination
 Sinatra::Delegator.target = app
 Sinatra::Delegator.delegate :widget
 
@@ -17,4 +18,5 @@ class Rack::Builder
   include Sinatra::Delegator
 end
 
+# Run the app _after_ the file (where this file was included in) was executed successfully to allow for configuration
 at_exit { Rack::Handler.default.run app }
